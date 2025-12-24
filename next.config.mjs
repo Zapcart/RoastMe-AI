2db60be74646b91bdbdbc66034f1d19fde949188
Zapcart <zapcartsuport@gmail.com>
2025-12-24 11:45:20 +0530

    Fix build issues (dynamic roast page)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
-
 
